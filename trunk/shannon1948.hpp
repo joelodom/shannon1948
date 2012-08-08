d05070ac537ad6ee0fa640eea0df08c8f0c56bc0
joelodom@gmail.com <joelodom@gmail.com@ab010d9d-24d9-fff9-2e84-9f375c671e15>
2012-08-08 15:49:58 +0000

    Added EntropySource and related tests.

--- trunk/shannon1948.hpp
+++ trunk/shannon1948.hpp
@@ -15,3 +15,40 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+namespace shannon1948
+{
+   typedef char SYMBOL;
+   typedef SYMBOL* MESSAGE; // an array of symbols
+
+   // EntropySource is a class to generate messages that have an expected amount
+   // of entropy.
+
+   class EntropySource
+   {
+   public:
+      // GenerateBinaryMessage generates a very simple message with two possible
+      // symbols.  p is the probability of generating the first symbol, so the
+      // probability of generating the second symbol is (1-p).  See Fig. 7 in
+      // Shannon's paper for a graph of how p will affect H.  The returned
+      // message should be freed with FreeMessage.
+      static MESSAGE GenerateBinaryMessage(double p, int length);
+
+      // Use FreeMessage to free a previously allocated message.
+      static void FreeMessage(MESSAGE message)
+      {
+         delete[] message;
+         ++s_memory_deallocations_;
+      }
+
+      // Use MemoryCheck to check that all allocated messages have been freed.
+      static bool MemoryCheck()
+      {
+         return s_memory_allocations_ == s_memory_deallocations_;
+      }
+
+   private:
+      static int s_memory_allocations_;
+      static int s_memory_deallocations_;
+   };
+}
