2c3d2b6353d0cd7927248c9d1aadf653ab70034e
joelodom <joelodom@gmail.com>
2012-08-08 15:49:58 +0000

    Added EntropySource and related tests.

--- shannon1948.cpp
+++ shannon1948.cpp
@@ -18,6 +18,34 @@
 
 #include "shannon1948.hpp"
 #include "gtest/gtest.h"
+#include <cstdlib>
+
+/* static */ int shannon1948::EntropySource::s_memory_allocations_ = 0;
+/* static */ int shannon1948::EntropySource::s_memory_deallocations_ = 0;
+
+/* static */ shannon1948::MESSAGE
+   shannon1948::EntropySource::GenerateBinaryMessage(double p, int length)
+{
+   // allocate the message
+   MESSAGE message = new SYMBOL[length];
+   ++s_memory_allocations_;
+
+   // populate the message
+   for (int i = 0; i < length; i++)
+   {
+      // TODO: Switch to a better random number generator / experiment with
+      // different generators.  Any experiment here is highly dependent on
+      // having a true random number source, so experiments may really only
+      // be measuring the entropy of the PRNG.
+
+      int r = rand(); // 0 to RAND_MAX, inclusive of both endpoints
+      while (r == RAND_MAX) // don't allow RAND_MAX
+         r = rand();
+      message[i] = rand() < p*RAND_MAX ? 'A' : 'B';
+   }
+
+   return message;
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
--- shannon1948_tests.cpp
+++ shannon1948_tests.cpp
@@ -19,7 +19,97 @@
 #include "shannon1948.hpp"
 #include "gtest/gtest.h"
 
-TEST(shannon1948_tests, gtest_test)
+TEST(overall_tests, gtest_test)
 {
    EXPECT_TRUE(true) << "Simple test of GTest failed.";
+}
+
+TEST(entropy_source_tests, memory_test)
+{
+   // make sure that allocations and deallocations work as expected
+   shannon1948::MESSAGE message
+      = shannon1948::EntropySource::GenerateBinaryMessage(0.5, 100);
+   shannon1948::EntropySource::FreeMessage(message);
+   EXPECT_TRUE(shannon1948::EntropySource::MemoryCheck())
+      << "Memory check failed.";
+}
+
+TEST(entropy_source_tests, test_p_zero)
+{
+   // p = 0.0 means that messages should be entirely Bs
+   const int LENGTH = 100;
+   shannon1948::MESSAGE message
+      = shannon1948::EntropySource::GenerateBinaryMessage(0.0, LENGTH);
+   for (int i = 0; i < LENGTH; i++)
+      ASSERT_TRUE(message[i] == 'B') << "Expected all Bs.";
+   shannon1948::EntropySource::FreeMessage(message);
+   EXPECT_TRUE(shannon1948::EntropySource::MemoryCheck())
+      << "Memory check failed.";
+}
+
+TEST(entropy_source_tests, test_p_one)
+{
+   // p = 1.0 means that messages should be entirely As
+   const int LENGTH = 100;
+   shannon1948::MESSAGE message
+      = shannon1948::EntropySource::GenerateBinaryMessage(1.0, LENGTH);
+   for (int i = 0; i < LENGTH; i++)
+      ASSERT_TRUE(message[i] == 'A') << "Expected all As.";
+   shannon1948::EntropySource::FreeMessage(message);
+   EXPECT_TRUE(shannon1948::EntropySource::MemoryCheck())
+      << "Memory check failed.";
+}
+
+TEST(entropy_source_tests, test_p_half)
+{
+   // p = 0.5 means that messages should contain As and Bs
+   const int LENGTH = 256;
+   shannon1948::MESSAGE message
+      = shannon1948::EntropySource::GenerateBinaryMessage(0.5, LENGTH);
+
+   bool found_a = false;
+   bool found_b = false;
+
+   for (int i = 0; i < LENGTH; i++)
+   {
+      if (message[i] == 'A')
+         found_a = true;
+      if (message[i] == 'B')
+         found_b = true;
+      if (found_a && found_b)
+         break;
+   }
+
+   EXPECT_TRUE(found_a && found_b) << "If all works as expected, "
+      "the probability of this test failing is negligible";
+
+   shannon1948::EntropySource::FreeMessage(message);
+   EXPECT_TRUE(shannon1948::EntropySource::MemoryCheck())
+      << "Memory check failed.";
+}
+
+TEST(entropy_source_tests, test_p_tenth)
+{
+   // p = 0.1 means that messages should contain mostly Bs
+   const int LENGTH = 256;
+   shannon1948::MESSAGE message
+      = shannon1948::EntropySource::GenerateBinaryMessage(0.1, LENGTH);
+
+   int a_count = 0;
+   int b_count = 0;
+
+   for (int i = 0; i < LENGTH; i++)
+   {
+      if (message[i] == 'A')
+         ++a_count;
+      if (message[i] == 'B')
+         ++b_count;
+   }
+
+   EXPECT_TRUE(b_count > a_count) << "If all works as expected, "
+      "the probability of this test failing is negligible";
+
+   shannon1948::EntropySource::FreeMessage(message);
+   EXPECT_TRUE(shannon1948::EntropySource::MemoryCheck())
+      << "Memory check failed.";
 }
