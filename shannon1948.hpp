f42b986bb52a9e6d62af596934145356d66881b1
joelodom <joelodom@gmail.com>
2012-08-10 21:32:07 +0000

    Switched to std::string for messages.  Began work on EntropyCalculator.

--- shannon1948.hpp
+++ shannon1948.hpp
@@ -16,39 +16,37 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include <string>
+
 namespace shannon1948
 {
-   typedef char SYMBOL;
-   typedef SYMBOL* MESSAGE; // an array of symbols
-
    // EntropySource is a class to generate messages that have an expected amount
    // of entropy.
 
    class EntropySource
    {
    public:
+
       // GenerateBinaryMessage generates a very simple message with two possible
       // symbols.  p is the probability of generating the first symbol, so the
       // probability of generating the second symbol is (1-p).  See Fig. 7 in
-      // Shannon's paper for a graph of how p will affect H.  The returned
-      // message should be freed with FreeMessage.
-      static MESSAGE GenerateBinaryMessage(double p, int length);
+      // Shannon's paper for a graph of how p will affect H.
+      static void GenerateBinaryMessage(
+         double p, size_t length, std::string& output);
+   };
 
-      // Use FreeMessage to free a previously allocated message.
-      static void FreeMessage(MESSAGE message)
-      {
-         delete[] message;
-         ++s_memory_deallocations_;
-      }
+   // EntropyCalculator uses statistical methods based on the section of
+   // Shannon's paper "The Entropy of an Information Source" to estimate
+   // the entropy contained in a message.
 
-      // Use MemoryCheck to check that all allocated messages have been freed.
-      static bool MemoryCheck()
-      {
-         return s_memory_allocations_ == s_memory_deallocations_;
-      }
+   class EntropyCalculator
+   {
+   public:
 
-   private:
-      static int s_memory_allocations_;
-      static int s_memory_deallocations_;
+      // G_N uses the simpler, but less precise formula to calculate entropy.
+      // G_N = -(1/N)*sum(p(B_i)*log2(p(B_i))), where the sum is over all
+      // sequences B_i containing N symbols.  As N grows, the limit approaches
+      // the entropy, H.
+      static double G_N(std::string message, size_t N);
    };
 }
